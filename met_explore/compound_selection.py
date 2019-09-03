d143e68305790a802a04bacd5270b38ac6f0e611
Karen McLuskey <Karen.McLuskey@me.com>
2019-09-03 15:06:55 +0100

    Add extra cmpd details to metabolite pop-up

--- met_explore/compound_selection.py
+++ met_explore/compound_selection.py
@@ -313,7 +313,7 @@ class CompoundSelector(object):
         cmpd = Compound.objects.get(id=cmpd_id)
         annot = Annotation.objects.get(peak=peak,compound=cmpd)
 
-        compound_details = {'hmdb_id':cmpd.get_hmdb_id(), 'kegg_id':cmpd.get_kegg_id(),'mz':peak.m_z, 'mass': annot.neutral_mass,
+        compound_details = {'inchikey': cmpd.inchikey, 'smiles': cmpd.smiles, 'cas_code': cmpd.cas_code, 'chebi_id': cmpd.chebi_id, 'hmdb_id':cmpd.get_hmdb_id(), 'kegg_id':cmpd.get_kegg_id(),'mz':peak.m_z, 'mass': annot.neutral_mass,
                             'rt': peak.rt, 'formula': cmpd.cmpd_formula, 'adduct': annot.adduct, 'name': cmpd.cmpd_name,
                             'identified': annot.identified, 'frank_annots': annot.frank_anno}
 
